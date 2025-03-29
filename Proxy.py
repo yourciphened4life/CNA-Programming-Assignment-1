8867341981bde0e2688f6337b64f038130e7e362
yourciphened4life <rahulrbud99@gmail.com>
2025-03-29 06:10:07 -0400

    Proxy attempts to connect to an invalid address due to the presence of leading slash before processing the URI

--- Proxy.py
+++ Proxy.py
@@ -16,6 +16,9 @@ args = parser.parse_args()
 proxyHost = args.hostname
 proxyPort = int(args.port)
 
+# ---------------
+# STEP 1: CREATE / BIND / LISTEN
+# ---------------
 try:
   # Create a server socket
   # ~~~~ INSERT CODE ~~~~
@@ -39,35 +42,133 @@ except:
 try:
   # Listen on the server socket
   # ~~~~ INSERT CODE ~~~~
-  serverSocket.listen(50)  # You can set the backlog to an appropriate value
+  serverSocket.listen(50)  # backlog of 50, adjust as needed
   # ~~~~ END CODE INSERT ~~~~
   print('Listening to socket')
 except:
   print('Failed to listen')
   sys.exit()
 
-# Continuously accept connections
+# ---------------
+# STEP 2: ACCEPT CLIENT & FORWARD REQUEST
+# ---------------
 while True:
   print('Waiting for connection...')
   clientSocket = None
 
   try:
-    # Accept connection from client and store in clientSocket
+    # Accept connection from client
     # ~~~~ INSERT CODE ~~~~
     clientSocket, clientAddress = serverSocket.accept()
     # ~~~~ END CODE INSERT ~~~~
-    print('Received a connection from:', clientAddress)
+    print(f'Received a connection from: {clientAddress}')
   except:
     print('Failed to accept connection')
     sys.exit()
 
-  # At this point, the proxy server is connected to the client
-  # (You can proceed with reading the request, etc.)
-
-  # For now, we can simply close the client socket right away
-  # to demonstrate that the connection was accepted.
+  # ---------------
+  # READ CLIENT REQUEST
+  # ---------------
   try:
-    clientSocket.close()
-    print('Closed the client socket.\n')
-  except:
-    print('Failed to close client socket')
+    # ~~~~ INSERT CODE ~~~~
+    message_bytes = clientSocket.recv(BUFFER_SIZE)
+    if not message_bytes:
+      print("No data received, closing connection.")
+      clientSocket.close()
+      continue
+
+    message = message_bytes.decode('utf-8', errors='replace')
+    # ~~~~ END CODE INSERT ~~~~
+
+    print('Received request:')
+    print('< ' + message)
+
+    # Parse the first line of the HTTP request (method, URI, version)
+    request_line = message.split('\r\n')[0]
+    requestParts = request_line.split()
+    method = requestParts[0]
+    URI = requestParts[1]
+    version = requestParts[2]
+
+    print('Method:\t\t' + method)
+    print('URI:\t\t' + URI)
+    print('Version:\t' + version)
+    print('')
+
+    # ---------------
+    # EXTRACT HOSTNAME AND RESOURCE
+    # ---------------
+    # Remove leading http:// or https://
+    URI = re.sub('^http(s?)://', '', URI, count=1)
+
+    # Split hostname from resource name
+    resourceParts = URI.split('/', 1)
+    hostname = resourceParts[0]
+    resource = '/'
+    if len(resourceParts) == 2:
+      resource = '/' + resourceParts[1]
+
+    print('Requested Resource:\t' + resource)
+
+    # ---------------
+    # CONNECT TO ORIGIN SERVER
+    # ---------------
+    try:
+      originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      address = socket.gethostbyname(hostname)
+      # By default, assume port 80 unless the URL indicates otherwise
+      originPort = 80
+
+      print('Connecting to:\t\t' + hostname)
+      originServerSocket.connect((address, originPort))
+      print('Connected to origin server')
+
+      # ---------------
+      # FORWARD CLIENT REQUEST TO ORIGIN SERVER
+      # ---------------
+      # Minimal HTTP request with Host header
+      originServerRequest = f"{method} {resource} {version}\r\n"
+      originServerRequest += f"Host: {hostname}\r\n"
+      originServerRequest += "Connection: close\r\n"
+      originServerRequest += "\r\n"
+
+      print('Forwarding request to origin server:')
+      for line in originServerRequest.split('\r\n'):
+        if line:
+          print('> ' + line)
+
+      originServerSocket.sendall(originServerRequest.encode())
+      print('Request sent to origin server\n')
+
+      # ---------------
+      # READ RESPONSE FROM ORIGIN SERVER
+      # ---------------
+      response_data = b''
+      while True:
+        chunk = originServerSocket.recv(BUFFER_SIZE)
+        if not chunk:
+          break
+        response_data += chunk
+
+      # ---------------
+      # SEND RESPONSE BACK TO CLIENT
+      # ---------------
+      clientSocket.sendall(response_data)
+      print('Sent response to client')
+
+      # Close origin server socket
+      originServerSocket.close()
+
+    except OSError as err:
+      print('Origin server request failed. ' + str(err))
+
+  except Exception as e:
+    print('Error while processing request:', e)
+
+  finally:
+    # Close the client socket for this request
+    try:
+      clientSocket.close()
+      print('Closed client socket\n')
+    except:
+      print('Failed to close client socket')
